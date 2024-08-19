15106bb7771fee110015c8258a8f8ffef6d47a77
Leo Bastos <lsbastos@gmail.com>
2024-08-19 16:19:00 -0300

    forecast 2024-2025

--- code/sprint_forecast_2024-2025.r
+++ code/sprint_forecast_2024-2025.r
@@ -5,10 +5,10 @@ source("code/sprint_fun.R")
 
 dengue <- read_csv("~/Dropbox/Research/Mosqlimate/Sprint/data/dengue.csv.gz")
 
-selufs <- c('AM', 'CE', 'GO', 'GO', 'MG', 'PR')
+# selufs <- c('AM', 'CE', 'GO', 'GO', 'MG', 'PR')
 
-dengue <- dengue %>% 
-  filter(uf %in% selufs)
+# dengue <- dengue %>% 
+#   filter(uf %in% selufs)
 
 dengue.tbl <- dengue %>% 
   group_by(uf, macroregional_geocode, macroregional) %>% tally() %>% 
@@ -26,42 +26,35 @@ set.seed(123456)
 
 macros <- unique(dengue$macroregional_geocode)
 
-list.train.1 = vector(mode = "list", length = length(macros))
-names(list.train.1) = macros
+list.train.3 = vector(mode = "list", length = length(macros))
+names(list.train.3) = macros
 
-list.train.2 = list.train.1
 
-#k = 1
+k = 1
 for(k in 1:length(macros)){
-  # Training 1
-  data.train1.macro.k = dengue %>% 
+  # Training 3
+  data.train3.macro.k = dengue %>% 
     filter(
-      macroregional_geocode == macros[k], 
-      (train_1 == T | target_1 == T)
+      macroregional_geocode == macros[k]
       # (train_2 == T | target_2 == T) 
     ) %>% 
     group_by(Date = date, macroregional_geocode, uf) %>% 
     summarise(
       cases = sum(casos),
-      train = train_1[1], target = target_1[1], .groups = "drop"
+      train = T, target = F, .groups = "drop"
       # train = train_2[1], target = target_2[1]
     ) 
   
-  data.train2.macro.k = dengue %>% 
-    filter(
-      macroregional_geocode == macros[k], 
-      (train_2 == T | target_2 == T)
-    ) %>% 
-    group_by(Date = date, macroregional_geocode, uf) %>% 
-    summarise(
-      cases = sum(casos),
-      train = train_2[1], target = target_2[1], .groups = "drop"
-    ) 
-  
+  aux.train3 <- tibble(Date = seq(from = ymd("2024-10-06"), by = 7, length.out = 52), 
+                       train = F, target = T,
+                       macroregional_geocode = data.train3.macro.k$macroregional_geocode[1],
+                       uf = data.train3.macro.k$uf[1]
+  )
   
   
   aux = forecasting.inla(dados = data.train1.macro.k %>% 
-                           filter(Date >= "2015-10-11"), 
+                           filter(Date >= "2015-10-11") %>% 
+                         bind_rows(aux.train3), 
                          MC =T)
   
   aux$pred$uf = data.train1.macro.k$uf[1]
@@ -70,18 +63,7 @@ for(k in 1:length(macros)){
   aux$MC$uf = data.train1.macro.k$uf[1]
   aux$MC$macrocode = data.train1.macro.k$macroregional_geocode[1]
   
-  list.train.1[[k]]$out <- aux
-  
-  aux2 <- forecasting.inla(dados = data.train2.macro.k %>% 
-                             filter(Date >= "2015-10-11"), 
-                           MC =T)
-  aux2$pred$uf = data.train2.macro.k$uf[1]
-  aux2$pred$macrocode = data.train2.macro.k$macroregional_geocode[1]
-  
-  aux2$MC$uf = data.train2.macro.k$uf[1]
-  aux2$MC$macrocode = data.train2.macro.k$macroregional_geocode[1]
-  
-  list.train.2[[k]]$out <- aux2
+  list.train.3[[k]]$out <- aux
   
   cat(k, data.train1.macro.k$uf[1] , data.train1.macro.k$macroregional_geocode[1], "\n")
   
@@ -157,28 +139,28 @@ tbl.total.uf.week.train1 <- df.train.1 %>%
       summarise(cases = sum(casos)), by = c("uf", "week")
   ) %>% 
   left_join(dt_train1)
-  # bind_rows(
-  #   tibble(uf = "BR") %>% 
-  #     bind_cols(df.train.1 %>% 
-  #                 group_by(week, samples) %>% 
-  #                 summarise(
-  #                   values = sum(values)
-  #                 ) %>% group_by(week) %>% 
-  #                 summarise(
-  #                   est = median(values),
-  #                   li = quantile(values, probs = 0.05),
-  #                   lu = quantile(values, probs = 0.95),
-  #                 ) %>% left_join(
-  #                   dengue %>% 
-  #                     filter(target_1 == T) %>% 
-  #                     mutate(
-  #                       week = week.season(date)
-  #                     ) %>% 
-  #                     group_by(week) %>%
-  #                     summarise(cases = sum(casos)), by = "week"
-  #                 )
-  #     )
-  # )
+# bind_rows(
+#   tibble(uf = "BR") %>% 
+#     bind_cols(df.train.1 %>% 
+#                 group_by(week, samples) %>% 
+#                 summarise(
+#                   values = sum(values)
+#                 ) %>% group_by(week) %>% 
+#                 summarise(
+#                   est = median(values),
+#                   li = quantile(values, probs = 0.05),
+#                   lu = quantile(values, probs = 0.95),
+#                 ) %>% left_join(
+#                   dengue %>% 
+#                     filter(target_1 == T) %>% 
+#                     mutate(
+#                       week = week.season(date)
+#                     ) %>% 
+#                     group_by(week) %>%
+#                     summarise(cases = sum(casos)), by = "week"
+#                 )
+#     )
+# )
 
 
 
@@ -236,47 +218,31 @@ tbl.total.uf.week.train2 <- df.train.2 %>%
       summarise(cases = sum(casos)), by = c("uf", "week")
   ) %>%  
   left_join(dt_train2)
-  # bind_rows(
-  #   tibble(uf = "BR") %>% 
-  #     bind_cols(df.train.2 %>% 
-  #                 group_by(week, samples) %>% 
-  #                 summarise(
-  #                   values = sum(values)
-  #                 ) %>% group_by(week) %>% 
-  #                 summarise(
-  #                   est = median(values),
-  #                   li = quantile(values, probs = 0.05),
-  #                   lu = quantile(values, probs = 0.95),
-  #                 ) %>% left_join(
-  #                   dengue %>% 
-  #                     filter(target_2 == T) %>% 
-  #                     mutate(
-  #                       week = week.season(date)
-  #                     ) %>% 
-  #                     group_by(week) %>%
-  #                     summarise(cases = sum(casos)), by = "week"
-  #                 )
-  #     )
-  # )
+# bind_rows(
+#   tibble(uf = "BR") %>% 
+#     bind_cols(df.train.2 %>% 
+#                 group_by(week, samples) %>% 
+#                 summarise(
+#                   values = sum(values)
+#                 ) %>% group_by(week) %>% 
+#                 summarise(
+#                   est = median(values),
+#                   li = quantile(values, probs = 0.05),
+#                   lu = quantile(values, probs = 0.95),
+#                 ) %>% left_join(
+#                   dengue %>% 
+#                     filter(target_2 == T) %>% 
+#                     mutate(
+#                       week = week.season(date)
+#                     ) %>% 
+#                     group_by(week) %>%
+#                     summarise(cases = sum(casos)), by = "week"
+#                 )
+#     )
+# )
 
 
 write_csv(tbl.total.uf.train1, file = "~/Desktop/tbl.total.uf.train1.csv")
 write_csv(tbl.total.uf.train2, file = "~/Desktop/tbl.total.uf.train2.csv")
 write_csv(tbl.total.uf.week.train1, file = "~/Desktop/tbl.total.uf.week.train1.csv")
 write_csv(tbl.total.uf.week.train2, file = "~/Desktop/tbl.total.uf.week.train2.csv")
-
-
-tbl.total.uf.train1 %>% 
-  ggplot(aes(y = uf)) + 
-  geom_pointrange(aes(x = est, xmin = li, xmax = lu, color = "Estimates")) +
-  geom_point(aes(x = cases, color = "Observed")) + 
-  scale_x_log10() + 
-  theme_bw()
-
-tbl.total.uf.train2 %>% 
-  ggplot(aes(y = uf)) + 
-  geom_pointrange(aes(x = est, xmin = li, xmax = lu, color = "Estimates")) +
-  geom_point(aes(x = cases, color = "Observed")) + 
-  scale_x_log10() + 
-  theme_bw()
-
